6308851d4b22f4ad64177a43a04d60d0fee62c9b
lnuic <lnuic123@gmail.com>
2023-11-30 22:57:17 +0100

    jakarta validation exception fix

--- card-manager-service/src/main/java/com/exercise/card/manager/exception/handler/CardManagerExceptionHandler.java
+++ card-manager-service/src/main/java/com/exercise/card/manager/exception/handler/CardManagerExceptionHandler.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +19,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CardManagerExceptionHandler {
@@ -34,6 +37,13 @@ public class CardManagerExceptionHandler {
 
         return new ResponseEntity<>(new CardManagerError(exception.getErrorCode(), exception.getErrorMessages(),
                 LocalDateTime.now()), status);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, List<String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
